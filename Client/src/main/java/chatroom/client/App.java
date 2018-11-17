9c4927bfeec4ebbdb9eb213c542401dd945b68fd
Daniel Fernando Ferraz Pinto <a21250143@isec.pt>
2018-11-17 23:14:42 +0000

    start working on chat

--- Client/src/main/java/chatroom/client/App.java
+++ Client/src/main/java/chatroom/client/App.java
@@ -1,59 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chatroom.client;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.Socket;
+
+import javax.swing.JFrame;
 
 /**
  *
  * @author daniel
  */
 public class App {
-
-    public static int MAX_SIZE = 256;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        DatagramSocket s = null;
-        DatagramPacket pkt = null;
-        ObjectInputStream oin;
-        
-        Socket toMaster;
-        int tcpPortNumber;
-        Thread t;
-        
-        try{
-            
-            s = new DatagramSocket(5001);
-            
-            pkt = new DatagramPacket(new byte[MAX_SIZE], MAX_SIZE); 
-            s.receive(pkt);
-            
-            System.out.println("Recebi um packet!");
-            
-            oin = new ObjectInputStream(new ByteArrayInputStream(pkt.getData()));
-            tcpPortNumber = (Integer) oin.readObject();
-            
-            toMaster = new Socket(pkt.getAddress(), tcpPortNumber);
-//            t = new Thread(new App(), "Thread 1");
-//            t.start();
-            
-            
-        } catch (IOException ex) {
-            System.out.println("Erro com o socket!");
-        } catch (ClassNotFoundException ex) {
-            System.out.println("Nao existe a class pretendida");
-        }
+        ChatClient client = new ChatClient();
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        try {
+            client.run();
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
     }
-    
+
 }
--- Server/src/main/java/chatroom/server/App.java
+++ Server/src/main/java/chatroom/server/App.java
@@ -1,15 +1,21 @@
 package chatroom.server;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Handler;
+
 /**
  *
  * @author daniel
@@ -17,19 +23,35 @@ import java.util.Set;
 public class App {
 
     /**
+     * The port that the server listens on.
+     */
+    private static final int PORT = 9001;
+
+    /**
+     * The set of all names of clients in the chat room. Maintained so that we can
+     * check that new clients are not registering name already in use.
+     */
+    private static HashSet<String> names = new HashSet<String>();
+
+    /**
+     * The set of all the print writers for all the clients. This set is kept so we
+     * can easily broadcast messages.
+     */
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 
         InetAddress IP;
-        int port;
-        
+
         try {
             IP = InetAddress.getLocalHost();
             System.out.println("IP of my system is := " + IP.getHostAddress());
         } catch (UnknownHostException e) {
             e.printStackTrace();
-		}
+        }
 
         /**
          * Start dbHelper with default values
@@ -66,50 +88,119 @@ public class App {
         for (User u : users) {
             System.out.println(u.getId() + " " + u.getUsername());
         }
-        
-        
-        Socket toClient;
-        ServerSocket ss = null;
-        DatagramSocket ds = null;
-        DatagramPacket dpk;
-        ByteArrayOutputStream bout;
-        ObjectOutputStream oout;
-        
-        try{
-            
-            System.out.println("Vou mandar o meu porto para o cliente!\n");
-            
-            ss = new ServerSocket(0);
-            
-            bout = new ByteArrayOutputStream();
-            oout = new ObjectOutputStream(bout);
-            oout.writeObject(ss.getLocalPort());
-            oout.flush();
-            
-            dpk = new DatagramPacket(bout.toByteArray(), bout.size(), InetAddress.getByName("255.255.255.255"), 5001);
-            ds = new DatagramSocket();
-            ds.send(dpk);
-            
-            System.out.println("A aguardar a resposta do Cliente");
-            
-            try{
-                while(true){
-                    toClient = ss.accept();
-                    System.out.println("Recebi uma resposta!");
-                }
-            }catch(IOException ex){
-                System.out.println("Erro ao aceder ao socket!");
-            }
-        } catch (IOException ex) {
-            System.out.println("Nao consegui abrir o socket!");
-        }
-        
 
         /**
          * Close connection with database
          */
         dbHelper.close();
 
+        System.out.println("The chat server is running.");
+
+        ServerSocket listener = null;
+        try {
+            listener = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            while (true) {
+                new Handler(listener.accept()).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                listener.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * A handler thread class. Handlers are spawned from the listening loop and are
+     * responsible for a dealing with a single client and broadcasting its messages.
+     */
+    private static class Handler extends Thread {
+        private String name;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        /**
+         * Constructs a handler thread, squirreling away the socket. All the interesting
+         * work is done in the run method.
+         */
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        /**
+         * Services this thread's client by repeatedly requesting a screen name until a
+         * unique one has been submitted, then acknowledges the name and registers the
+         * output stream for the client in a global set, then repeatedly gets inputs and
+         * broadcasts them.
+         */
+        public void run() {
+            try {
+
+                // Create character streams for the socket.
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                // Request a name from this client. Keep requesting until
+                // a name is submitted that is not already used. Note that
+                // checking for the existence of a name and adding the name
+                // must be done while locking the set of names.
+                while (true) {
+                    out.println("SUBMITNAME");
+                    name = in.readLine();
+                    if (name == null) {
+                        return;
+                    }
+                    synchronized (names) {
+                        if (!names.contains(name)) {
+                            names.add(name);
+                            break;
+                        }
+                    }
+                }
+
+                // Now that a successful name has been chosen, add the
+                // socket's print writer to the set of all writers so
+                // this client can receive broadcast messages.
+                out.println("NAMEACCEPTED");
+                writers.add(out);
+
+                // Accept messages from this client and broadcast them.
+                // Ignore other clients that cannot be broadcasted to.
+                while (true) {
+                    String input = in.readLine();
+                    if (input == null) {
+                        return;
+                    }
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + name + ": " + input);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+                // This client is going down! Remove its name and its print
+                // writer from the sets, and close its socket.
+                if (name != null) {
+                    names.remove(name);
+                }
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
     }
 
 }

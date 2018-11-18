2e4a6155d498c0a064fa84949d764e751e22ad1e
Pedro Lanhoso <pelan05@gmail.com>
2018-11-18 22:27:12 +0000

    .minor Messsage update

--- ChatRoomLibrary/src/main/java/chatroomlibrary/Message.java
+++ ChatRoomLibrary/src/main/java/chatroomlibrary/Message.java
@@ -6,15 +6,24 @@ import java.time.LocalDateTime;
 public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    String sender;
-    String text;
-    LocalDateTime date;
+    private String sender;
+    private String text;
+    private LocalDateTime date;
 
     public Message(String user, String text){
         this.date = LocalDateTime.now();
         this.sender = user;
         this.text = text;
     }
+
+
+    public void setSender(String send){this.sender = send;}
+    public String getSender(){return this.sender;}
+    
+    public void setText(String tex){this.text = tex;}
+    public String getText(){return this.text;}
+    
+    public LocalDateTime getDate(){return this.date;}
 
 
 
